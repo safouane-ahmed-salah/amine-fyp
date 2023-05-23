391d855c62266430f8b8bb5f440332aab55aa731
safouane ahmed salah <safoa14@gmail.com>
2023-05-23 18:07:14 +0800

    change scanner

--- src/Admin/scan.js
+++ src/Admin/scan.js
@@ -1,33 +1,24 @@
 import { useState } from "react";
-// import { QrReader } from 'react-qr-reader';
-import QrReader from 'react-qr-scanner';
+import { QrReader } from 'react-qr-reader';
 
 export default function Scan(){
 const [data, setData] = useState('No result');
 
   return (
-    <>
+    <>   
       <QrReader
-        // onScan={(result, error) => {
-        //   if (!!result) {
-        //     setData(result?.text);
-        //   }
-
-        //   if (!!error) {
-        //     console.info(error);
-        //   }
-        // }}
-        // constraints={{
-        //     facingMode: 'environment'
-        // }}
-        style={{
-            height: 240,
-            width: 320,
+        onScan={(result, error) => {
+          if (!!result) {
+            setData(result?.text);
+          }
+          if (!!error) {
+            console.info(error);
+          }
         }}
-        delay={100}
-        // style={{ width: '100%' }}
-        onScan={setData}
-        facingMode="rear"
+        constraints={{
+            facingMode: 'environment'
+        }}
+        style={{ width: '100%' }}
       />
       <p>{data}</p>
     </>
